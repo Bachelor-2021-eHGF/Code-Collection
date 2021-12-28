7c7028c415c3ec33b9365d3fd0cb8bcadb5e0005
sorenorm <54844057+sorenorm@users.noreply.github.com>
2021-12-28 17:48:47 +0100

    Added scripts for stress test

---
+++ HGF_stress_test/HGF/tapas_sgm_muhat.m
@@ -0,0 +1,32 @@
+
+function y = tapas_sgm_muhat(x, a)
+% --------------------------------------------------------------------------------------------------
+% Copyright (C) 2012-2013 Christoph Mathys, TNU, UZH & ETHZ
+%
+% This file is part of the HGF toolbox, which is released under the terms of the GNU General Public
+% Licence (GPL), version 3. You can redistribute it and/or modify it under the terms of the GPL
+% (either version 3 or, at your option, any later version). For further details, see the file
+% COPYING or <http://www.gnu.org/licenses/>.
+
+% ./ Array right division. A./B is the matrix with elements A(i,j)/B(i,j). 
+% A and B must have the same size, unless one of them is a scalar. 
+
+% x = ka(1)*muhat
+% a = 1
+
+upper = double(0.999);
+lower = double(0.001);
+
+y = a/(1+exp(-x));
+
+% y = double(y);
+% 
+% if y > upper;
+%     y = upper;
+% elseif y < lower;
+%     y = lower;
+% end;
+
+
+return;
+
